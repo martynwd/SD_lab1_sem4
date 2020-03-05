48bee9178e76e8604d26da7e380a9c56eb699ba8
Martynov Daniil <dendimart@gmail.com>
2020-03-05 10:16:53 +0300

    add tests

--- test_circle.py
+++ test_circle.py
@@ -13,4 +13,20 @@ class TestCirlceArea(unittest.TestCase):
 
     def test_area(self):
         self.assertEqual(self.test_circle.area(), 78.53981633974483)
+        
+    
+    def test_perimetr(self):
         self.assertEqual(self.test_circle.perimetr(), 31.41592653589793)
+
+
+    def test_plusvector(self):
+        self.test_circle = self.test_circle.plusvector(self.test_point)
+        self.assertEqual(self.test_circle.x, 4)
+        self.assertEqual(self.test_circle.y, 4)
+
+    def test_initial_value(self):
+       self.assertEqual(self.test_circle.radius,5)
+       self.assertEqual(self.test_circle.center.x,self.test_point.x)
+       self.assertEqual(self.test_circle.center.y, self.test_point.y)
+        
+
